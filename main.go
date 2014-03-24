11452d1ec75e2199cecc6c26362aff6271153331
Rodolfo Granata <warlock.cc@gmail.com>
2014-03-23 22:22:14 -0400

    * adjust printing place

--- main.go
+++ main.go
@@ -4,16 +4,18 @@ import (
 	"bloom"
 	"bufio"
 	"flag"
-	"log"
+	"fmt"
 	"net/url"
 	"os"
 	"runtime"
+	"time"
 )
 
 var (
 	baseurl     = flag.String("baseurl", "", "start url")
 	numcrawlers = flag.Int("crawlers", 10, "num of concurrent crawlers")
 	maxqueue    = flag.Int("maxqueue", 1e+6, "max links to queue")
+	stats       = flag.Bool("stats", true, "print stats on stdout")
 )
 
 func init() {
@@ -21,11 +23,12 @@ func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 }
 
-func Filter(in <-chan []*url.URL) <-chan *url.URL {
+func Filter(in <-chan []*url.URL, printer chan<- string) <-chan *url.URL {
 	out := make(chan *url.URL, *maxqueue)
 	// kickoff
 	if _url, err := url.Parse(*baseurl); err != nil {
-		log.Fatalf("Couldn't parse baseurl: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Couldn't parse baseurl: %v\n", err)
+		os.Exit(1)
 	} else {
 		out <- _url
 	}
@@ -33,21 +36,32 @@ func Filter(in <-chan []*url.URL) <-chan *url.URL {
 	dups := 0
 	total := 0
 
+	if *stats {
+		go func() {
+			tmchan := time.Tick(time.Second * 2)
+			for {
+				<-tmchan
+				fmt.Fprintf(os.Stderr, "Total: %v, Dups: %v, qlen: %v\n", total, dups, len(out))
+			}
+		}()
+	}
+
 	filter := bloom.New(1e+7)
 	go func() {
 		for urls := range in {
 			for _, u := range urls {
 				total++
-				if ustr := u.String(); !filter.Has(ustr) {
+				if ustr := u.String(); filter.AddExisted(ustr) {
+					// most probably a dup, but could be filter's false-positive
+					dups++
+				} else {
+					printer <- u.String()
 					// Drop stuff if the queue is full
 					select {
 					case out <- u:
-						filter.Add(ustr)
 					default:
 						total--
 					}
-				} else {
-					dups++
 				}
 			}
 		}
@@ -58,13 +72,12 @@ func Filter(in <-chan []*url.URL) <-chan *url.URL {
 
 func main() {
 	pending := make(chan []*url.URL)
-	crawlqueue := Filter(pending)
 	printer := make(chan string)
+	crawlqueue := Filter(pending, printer)
 
 	for i := 0; i < *numcrawlers; i++ {
 		go func() {
 			for next := range crawlqueue {
-				printer <- next.String()
 				pending <- NewHtmlParser(next).ExtractLinks()
 			}
 		}()
